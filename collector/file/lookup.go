1d1fb60990052c74386f12c85d9c732ad1c93daa
amyangfei <amyangfei@gmail.com>
2016-03-14 23:46:35 +0800

    Update lookup files method in collector

--- collector/file/lookup.go
+++ collector/file/lookup.go
@@ -2,42 +2,68 @@ package file
 
 import (
 	"github.com/amyangfei/dmux/registry/models"
+	"github.com/amyangfei/dmux/utils"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
-func walk(dir string) ([]string, error) {
-	result := make([]string, 0)
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return result, err
+func walk(dir string) (rFiles, lnNames, lnTargets []string, err error) {
+	files, _err := ioutil.ReadDir(dir)
+	if _err != nil {
+		err = _err
+		return
 	}
 
 	for _, file := range files {
 		if !file.IsDir() {
 			if file.Mode().IsRegular() {
-				result = append(result, file.Name())
+				rFiles = append(rFiles, file.Name())
 			} else {
-				lnfile, err := os.Readlink(filepath.Join([]string{dir, file.Name()}...))
-				if err != nil {
-					return result, err
+				lnfile, _err := os.Readlink(filepath.Join([]string{dir, file.Name()}...))
+				if _err != nil {
+					err = _err
+					return
 				}
-				lnstat, err := os.Stat(lnfile)
-				if err != nil {
-					return result, err
+				lnstat, _err := os.Stat(lnfile)
+				if _err != nil {
+					err = _err
+					return
 				}
 				if !lnstat.IsDir() {
-					result = append(result, lnfile)
+					lnNames = append(lnNames, file.Name())
+					lnTargets = append(lnTargets, lnfile)
 				}
 			}
 		}
 	}
-	return result, nil
+	return
 }
 
 // LookupFiles find all candinate files according to the given entry
-func LookupFiles(entry *models.Entry) ([]string, error) {
-	files, err := walk(entry.Path)
-	return files, err
+func LookupFiles(entry *models.Entry) (result []string, err error) {
+	if len(entry.Include) > 0 {
+		for _, filename := range entry.Include {
+			result = append(
+				result, filepath.Join([]string{entry.Path, filename}...))
+		}
+	} else {
+		rFiles, lnNames, lnTargets, _err := walk(entry.Path)
+		if _err != nil {
+			err = _err
+			return
+		}
+		for _, filename := range rFiles {
+			if !utils.In(filename, entry.Exclude) {
+				result = append(
+					result, filepath.Join([]string{entry.Path, filename}...))
+			}
+		}
+		for i := 0; i < len(lnNames); i++ {
+			if !utils.In(lnNames[i], entry.Exclude) {
+				result = append(result, lnTargets[i])
+			}
+		}
+	}
+	return
 }
